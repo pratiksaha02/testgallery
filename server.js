c3340bd70243e65bce81f0252d64632848888261
pratiksaha02 <59769135+pratiksaha02@users.noreply.github.com>
2024-02-08 16:31:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/portfolio", {
+const mongoURI="mongodb+srv://02pratiksh:its a sunny day00@cluster0.ikywwba.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
